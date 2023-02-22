028ff6a70b0e19f22497be23ff3ffdab313f8533
Daniel Švub <daniel.svub@spongedata.cz>
2023-02-22 15:34:00 +0100

    Updated tests

--- stream_test.go
+++ stream_test.go
@@ -10,31 +10,31 @@ func TestFileStream(t *testing.T) {
 	t.Run("isstream", func(t *testing.T) {
 		_, err := gonativeextractor.NewFileStream("./fixtures/create_stream.txt")
 		if err != nil {
-			t.Errorf("Error unexpected")
+			t.Errorf("Stream creation failed.")
 		}
 	})
 
 	t.Run("nostream", func(t *testing.T) {
 		_, err := gonativeextractor.NewFileStream("./fixtures/nosuchfile.txt")
 		if err == nil {
-			t.Errorf("Error expected but not set")
+			t.Errorf("No error when trying to open non-existent file.")
 		}
 	})
 
 	t.Run("closestream", func(t *testing.T) {
 		s, _ := gonativeextractor.NewFileStream("./fixtures/create_stream.txt")
 		if s.Close() != nil {
-			t.Errorf("Error unexpected")
+			t.Errorf("Stream closing failed.")
 		}
 		if s.Close() == nil {
-			t.Errorf("Error expected but not set")
+			t.Errorf("No error when closed the second time.")
 		}
 	})
 }
 
 func TestBufferStream(t *testing.T) {
 	t.Run("create", func(t *testing.T) {
-		stream, err := gonativeextractor.NewBufferStream([]byte("test"))
+		stream, err := gonativeextractor.NewBufferStream([]byte("test\x00"))
 		if err != nil {
 			t.Errorf("Stream creation failed.")
 		}

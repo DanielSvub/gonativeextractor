cd3ca2db97d24a27b4bc943394f2d4e331d32d64
Tomas Urbanec <tomas.urbanec@upol.cz>
2023-11-15 07:44:43 +0100

    debug

--- extractor.go
+++ extractor.go
@@ -26,6 +26,7 @@ package gonativeextractor
    }
    bool extractor_c_add_miner_from_so_bridge(void *f, extractor_c * self, const char * miner_so_path, const char * miner_name, void * params)
    {
+     printf("bridge is bridgeing\n");
      return ((bool (*)(extractor_c *, const char *, const char *, void* ))f)(self, miner_so_path, miner_name, params);
    }
    const char * extractor_get_last_error_bridge(void * f, extractor_c * self)
